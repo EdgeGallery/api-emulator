edded1fdb3d6db2ddcd4eea645991f1fd4d1f296
Victor Gao <victor.gao@huawei.com>
2020-10-16 10:48:42 +0800

    ADD MP1 test cases
    Signed-off-by: Victor Gao <victor.gao@huawei.com>

---
+++ src/test/java/org/mec/emulator/controller/UploadApiTest.java
@@ -0,0 +1,69 @@
+/*
+ *    Copyright 2020 Huawei Technologies Co., Ltd.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.mec.emulator.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.apache.http.entity.ContentType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.multipart.MultipartFile;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EmulatorTests.class)
+@AutoConfigureMockMvc
+public class UploadApiTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void testUploadImageSuccess() throws Exception {
+        ClassPathResource resource = new ClassPathResource("location_api.yaml");
+        File sourceFile = resource.getFile();
+        InputStream sourceInputStream = new FileInputStream(sourceFile);
+
+        MultipartFile sourceMultiFile = new MockMultipartFile(sourceFile.getName(), sourceFile.getName(),
+            ContentType.APPLICATION_OCTET_STREAM.toString(), sourceInputStream);
+
+        ResultActions result = mvc.perform(
+            MockMvcRequestBuilders.multipart("/v1/face-recognition/upload")
+                .file("file", sourceMultiFile.getBytes())).andExpect(MockMvcResultMatchers.status().isOk())
+            .andDo(MockMvcResultHandlers.print());
+        Assert.assertEquals(result.andReturn().getResponse().getStatus(), 200);
+
+    }
+
+}
