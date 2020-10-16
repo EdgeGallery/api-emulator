da8ed13718d2eab90c99a9e77aa8624cf9459870
pangshihao <pangshihao@huawei.com>
2020-10-16 10:22:26 +0800

    Add Face recongnition API test cases
    Signed-off-by: pangshihao <pangshihao@huawei.com>

---
+++ src/test/java/org/mec/emulator/controller/DefaultApiTest.java
@@ -0,0 +1,53 @@
+/*
+ *    Copyright 2020 Huawei Technologies Co., Ltd.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.mec.emulator.controller;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EmulatorTests.class)
+@AutoConfigureMockMvc
+public class DefaultApiTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Before
+    public void init() {
+    }
+
+    @Test
+    public void testHealthCheckSuccess() throws Exception {
+        ResultActions result = mvc.perform(MockMvcRequestBuilders.get("/v1/face-recognition/"))
+            .andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());
+        Assert.assertEquals(result.andReturn().getResponse().getStatus(), 200);
+
+    }
+
+}
